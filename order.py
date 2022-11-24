0285fec68880c93941ebe81074c70bcbb82fcee5
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-11-24 17:19:46 +0500

    Практический пример : переработка паттерна Стратегия, Классическая Стратегия

---
+++ order.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from decimal import Decimal
+from typing import NamedTuple, Optional
+
+
+class Customer(NamedTuple):
+    name: str
+    fidelity: int
+
+
+class LineItem(NamedTuple):
+    product: str
+    quantity: int
+    price: Decimal
+
+    def total(self) -> Decimal:
+        return self.price * self.quantity
+
+
+class Order(NamedTuple):    # контекст
+    customer: Customer
+    cart: Sequence[LineItem]
+    promotion: Optional['Promotion'] = None
+
+    def total(self) -> Decimal:
+        totals = (item.total() for item in self.cart)
+        return sum(totals, start=Decimal(0))
+
+    def due(self) -> Decimal:
+        if self.promotion is None:
+            discount = Decimal(0)
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        return f'<Order total: {self.total():.2f} due: {self.due():.2f}>'
+
+
+class Promotion(ABC):   # Стратегия: абстрактный базовый класс
+    @abstractmethod
+    def discount(self, order: Order) -> Decimal:
+        """Вернуть скидку в виде положительной суммы в долларах"""
